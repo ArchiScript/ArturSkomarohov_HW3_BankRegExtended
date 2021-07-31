53a55421c7e1629bb7270d7a9babef29cd6a130b
Артур <arturskomarohov@gmail.com>
2021-07-31 23:11:34 +0300

    поиск по списку на Linq

---
+++ BankRegExtended/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using System.Linq;
+
+
+namespace BankRegExtended
+{
+    public class Client 
+    {
+        public string Name { get; set; }
+        public string DateOfBirth { get; set; }
+        public string PassNumber { get; set; }
+        public ulong AccNumber { get; set; }
+        public decimal AccSum { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Client))
+            {
+                return false;
+            }
+            Client result = (Client)obj;
+            return result.Name == Name &&
+                result.DateOfBirth == DateOfBirth &&
+                result.PassNumber == PassNumber &&
+                result.AccNumber == AccNumber&&
+                result.AccSum == AccSum;
+        }
+        public static bool operator ==(Client first, Client second)
+        {
+            return first.Equals(second);
+        }
+        public static bool operator !=(Client first, Client second)
+        {
+            return !first.Equals(second);
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + DateOfBirth.GetHashCode() +
+                PassNumber.GetHashCode() + AccNumber.GetHashCode()+
+                AccSum.GetHashCode();
+        }
+
+    }
+}
--- BankRegExtended/Program.cs
+++ BankRegExtended/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 
 namespace BankRegExtended
 {
@@ -6,7 +11,79 @@ namespace BankRegExtended
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Client> clients = new List<Client>();
+            clients.Add(new Client
+            {
+                Name = "Василий Александрович Петров",
+                DateOfBirth = "25.05.1975",
+                PassNumber = "I-ПР012345",
+                AccNumber = 0230000000123456,
+                AccSum = 500
+            });
+            clients.Add(new Client
+            {
+                Name = "Dan Brown",
+                DateOfBirth = "23.09.1985",
+                PassNumber = "I-ПР055345",
+                AccNumber = 0230000000143456,
+                AccSum = 2500
+            });
+            clients.Add(new Client
+            {
+                Name = "Егор Борисович Брынза",
+                DateOfBirth = "01.02.1963",
+                PassNumber = "I-ПР058845",
+                AccNumber = 0230000000178456,
+                AccSum = 630
+            });
+            clients.Add(new Client
+            {
+                Name = "Алиса Макаровна Шашли",
+                DateOfBirth = "05.08.2001",
+                PassNumber = "I-ПР753845",
+                AccNumber = 0230000000278456,
+                AccSum = 2365
+            });
+
+
+            var findName =
+                from client in clients
+                where client.Name == "Василий Александрович Петров"
+                orderby client.AccSum descending
+                select client;
+            foreach (var cl in findName)
+            {
+                Console.WriteLine($"Найден клиент {cl.DateOfBirth}");
+            }
+
+
+            var lowerSum =
+                from client in clients
+                where client.AccSum < 1000
+                select client;
+            foreach (var cl in lowerSum)
+            {
+                Console.WriteLine($"Сумма меньше 1000$:   {cl.Name} {cl.PassNumber} {cl.AccSum}");
+            }
+            Dictionary<string, decimal> sumCollection =
+            new Dictionary<string, decimal>();
+
+            
+            foreach (var cl in clients)
+            {
+                sumCollection.Add(cl.Name, cl.AccSum);
+            }
+            decimal lowestFromVal = sumCollection.Values.Min();
+            foreach (var cl in sumCollection)
+            {
+                if(cl.Value == lowestFromVal) { Console.WriteLine($"{cl.Key} {cl.Value}"); }
+            }
+
+            decimal overall = sumCollection.Values.Sum();
+            Console.WriteLine(overall);
+
         }
+
+
     }
 }
