986b16c6d8b6729adf87d049e177f40864e8c05a
Артур <arturskomarohov@gmail.com>
2021-08-02 19:52:38 +0300

    изменил методы с листа на дикт

--- BankRegExtended/Program.cs
+++ BankRegExtended/Program.cs
@@ -10,48 +10,42 @@ namespace BankRegExtended
     {
         public static void Main(string[] args)
         {
-            List<Client> clients = new List<Client>();
+            Dictionary<Client, Account> clients = new Dictionary<Client, Account>();
             clients.Add(new Client
             {
                 Name = "Василий Александрович Петров",
                 DateOfBirth = "25.05.1975",
                 PassNumber = "I-ПР012345",
-                AccData = new Account { AccNumber = 0230000000563456, CurrencyType = new RUB (), Ammount = 460 },
-                //AccSum = 500
-            });
+                
+            }, new Account { AccNumber = 0230000000563456, CurrencyType = new RUB(), Ammount = 460 });
             clients.Add(new Client
             {
                 Name = "Dan Brown",
                 DateOfBirth = "23.09.1985",
                 PassNumber = "I-ПР055345",
-                AccData = new Account { AccNumber = 0230000000143456, CurrencyType = new RUB(), Ammount = 1200 },
-                
-                //AccSum = 2500
-            });
+               
+            }, new Account { AccNumber = 0230000000143456, CurrencyType = new RUB(), Ammount = 1200 });
             clients.Add(new Client
             {
                 Name = "Егор Борисович Брынза",
                 DateOfBirth = "01.02.1963",
                 PassNumber = "I-ПР058845",
-                AccData = new Account { AccNumber = 0230000000178456, CurrencyType = new RUB(), Ammount = 8700},
-                //AccSum = 630
-            });
+                               
+            }, new Account { AccNumber = 0230000000178456, CurrencyType = new RUB(), Ammount = 8700 });
             clients.Add(new Client
             {
                 Name = "Алиса Макаровна Шашли",
                 DateOfBirth = "05.08.2001",
                 PassNumber = "I-ПР753845",
-                AccData = new Account { AccNumber = 0230000000278456, CurrencyType = new RUB(), Ammount = 1200 },
-                //AccSum = 2365
-            });
+                
+            }, new Account { AccNumber = 0230000000278456, CurrencyType = new RUB(), Ammount = 1200 });
             clients.Add(new Client
             {
                 Name = "Изя Вольфович Шниперсон",
                 DateOfBirth = "14.06.1958",
                 PassNumber = "I-ПР753822",
-                AccData = new Account { AccNumber = 0230000000778456, CurrencyType = new USD(), Ammount = 2300 },
-                //AccSum = 33568
-            });
+                
+            }, new Account { AccNumber = 0230000000778456, CurrencyType = new USD(), Ammount = 2300 });
 
 
             //Поиск по номеру паспорта
@@ -74,51 +68,51 @@ namespace BankRegExtended
 
 
         }
-        public static void DisplayClientByPass(List<Client> clients, string passNumber)
+        public static void DisplayClientByPass(Dictionary<Client,Account> clients, string passNumber)
         {
             var findName =
           from client in clients
-          where client.PassNumber == passNumber
+          where client.Key.PassNumber == passNumber
           select client;
 
             foreach (var item in findName)
             {
-                Console.WriteLine($" Найдены данные по номеру пасспорта {passNumber}: {item.Name} {item.AccData.Ammount} \n");
+                Console.WriteLine($" Найдены данные по номеру пасспорта {passNumber}: {item.Key.Name} {item.Value.Ammount} \n");
             }
         }
-        public static void DisplayClientByName(List<Client> clients, string name)
+        public static void DisplayClientByName(Dictionary<Client, Account> clients, string name)
         {
             var findName =
           from client in clients
-          where client.Name == name
+          where client.Key.Name == name
           select client;
 
             foreach (var item in findName)
             {
-                Console.WriteLine($" Найдены данные по ФИО {name}: {item.AccData} {item.AccData.Ammount} \n");
+                Console.WriteLine($" Найдены данные по ФИО {name}: {item.Value.AccNumber} {item.Value.AccNumber} \n");
             }
         }
 
-        public static void DisplayClientUnderSum(List<Client> clients, decimal sum)
+        public static void DisplayClientUnderSum(Dictionary<Client,Account> clients, decimal sum)
         {
             var lowerSum =
                from client in clients
-               where client.AccData.Ammount < sum
+               where client.Value.Ammount < sum
                select client;
             foreach (var cl in lowerSum)
             {
-                Console.WriteLine($" Сумма меньше {sum}$:   {cl.Name} {cl.PassNumber} {cl.AccData.Ammount} \n");
+                Console.WriteLine($" Сумма меньше {sum}$:   {cl.Key.Name} {cl.Key.PassNumber} {cl.Value.Ammount} \n");
             }
         }
 
-        public static void DisplayLowestSumClient(List<Client> clients)
+        public static void DisplayLowestSumClient(Dictionary<Client,Account> clients)
         {
             Dictionary<string, decimal> sumCollection =
             new Dictionary<string, decimal>();
 
             foreach (var cl in clients)
             {
-                sumCollection.Add(cl.Name, cl.AccData.Ammount);
+                sumCollection.Add(cl.Key.Name, cl.Value.Ammount);
             }
             decimal lowestFromVal = sumCollection.Values.Min();
             foreach (var cl in sumCollection)
@@ -130,27 +124,27 @@ namespace BankRegExtended
             }
         }
 
-        public static void CountAllMoney(List<Client> clients)
+        public static void CountAllMoney(Dictionary<Client,Account> clients)
         {
             Dictionary<string, decimal> sumCollection =
             new Dictionary<string, decimal>();
 
             foreach (var cl in clients)
             {
-                sumCollection.Add(cl.Name, cl.AccData.Ammount);
+                sumCollection.Add(cl.Key.Name, cl.Value.Ammount);
             }
             decimal totalSum = sumCollection.Values.Sum();
             Console.WriteLine($" Общая сумма остатков на счетах клиентов банка: {totalSum} \n");
         }
 
-        public static void DisplayYoungClient(List<Client> clients)
+        public static void DisplayYoungClient(Dictionary<Client,Account> clients)
         {
             Dictionary<DateTime, string> birthDays =
             new Dictionary<DateTime, string>();
 
             foreach (var cl in clients)
             {
-                birthDays.Add(DateTime.Parse(cl.DateOfBirth), cl.Name);
+                birthDays.Add(DateTime.Parse(cl.Key.DateOfBirth), cl.Key.Name);
             }
 
             foreach (var cl in birthDays)
